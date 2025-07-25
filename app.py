9720366b0a4a8cfb4a908e7fac2e3087efaf34ca
MasonScarby <madoncs522@gmail.com>
2025-07-25 14:56:05 -0400

    Working sqllite queries and requests

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from routes import init_routes
+import os
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-init_routes(app)
+# Example model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+# Create database and tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return 
+
+@app.route('/add_test')
+def add_test():
+    with app.app_context():
+        test_user = User(name='Test User')
+        db.session.add(test_user)
+        db.session.commit()
+    return 'Test user added!'
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create database tables
     app.run(debug=True)
